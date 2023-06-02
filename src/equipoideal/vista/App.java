dcf5ad2f62aec089695018c59f8d9912eee93e61
MatiasMoralezC <matiasmoralezc@gmail.com>
2023-06-02 18:25:53 -0300

    Personalizando vista

--- src/equipoideal/vista/App.java
+++ src/equipoideal/vista/App.java
@@ -3,10 +3,17 @@ package equipoideal.vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Cursor;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
 
 public class App {
 
-	private JFrame frame;
+	private JFrame frmTrabajoPracticoIii;
 
 	/**
 	 * Launch the application.
@@ -16,7 +23,7 @@ public class App {
 			public void run() {
 				try {
 					App window = new App();
-					window.frame.setVisible(true);
+					window.frmTrabajoPracticoIii.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +42,16 @@ public class App {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTrabajoPracticoIii = new JFrame();
+		frmTrabajoPracticoIii.setTitle("Programación III");
+		frmTrabajoPracticoIii.setBounds(100, 100, 800, 600);
+		frmTrabajoPracticoIii.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTrabajoPracticoIii.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Software Factory Team");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(268, 11, 267, 36);
+		lblNewLabel.setFont(new Font("Mistral", Font.PLAIN, 35));
+		frmTrabajoPracticoIii.getContentPane().add(lblNewLabel);
 	}
-
 }
