e7c4f6f9523c0598d4e7d8ed540f73d0b73b4536
maceratomas <maceratomas@gmail.com>
2023-06-05 23:55:59 -0300

    implemente los test de cargarIncompatibles

---
+++ src/equipoideal/modelo/EquipoTest.java
@@ -0,0 +1,92 @@
+package equipoideal.modelo;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EquipoTest {
+	/*
+	 * void cargarEmpleado(Empleado e) --> hace falta?
+	 * 
+	 * void cargarIncompatibles(String nombre1, String nombre2)
+	 * 
+	 * boolean verificarEmpleado(String nombre)
+	 * 
+	 * quitarIncompatibilidad(int index)
+	 * 
+	 * List<Empleado> encontrarEquipoSinConflictos(List<Empleado> empleados,
+	 *  	List<List<Empleado>> incompatibles, HashMap<Rol, Integer> rolesRequeridos) -- luego
+	 *  
+	 * List<Empleado> filtrarEmpleadosNoRequeridos(List<Empleado> empleados,
+	 * 		HashMap<Rol, Integer> rolesRequeridos)
+	 * 
+	 * int contarEmpleadosRequeridosCumplidos(HashMap<Rol, Integer> rolesRequeridos, List<Empleado> equipo)
+	 * 
+	 * double obtenerCalificacionTotal(List<Empleado> equipo)
+	 * 
+	 * List<List<Empleado>> generarCombinaciones(List<Empleado> empleados, int r)
+	 * 
+	 * void generarCombinacionesRecursivo(List<Empleado> empleados, int r, int index, List<Empleado> combinacionActual, List<List<Empleado>> result)
+	 * 
+	 * boolean tieneConflictos(List<Empleado> equipo, List<List<Empleado>> incompatibles)
+	 * 
+	 * boolean verificarRequerimiento(HashMap<Rol, Integer> requerimientos, Empleado e)
+	 * 
+	 * Empleado buscarPorNombre(String nombre)
+	 */
+	
+
+	Equipo equipo;
+	@Before
+	public void inicializar() {
+		equipo = new Equipo();
+		
+		String[] nombres = {"pedro","pipo","picasso","pablo"};
+		Rol[] rolesDeEmpleados = {Rol.LIDER_DE_PROYECTO, Rol.ARQUITECTO, Rol.PROGRAMADOR, Rol.TESTER};
+		int[] calificaciones = {5,4,3,3};
+
+		for (int i = 0; i < nombres.length; i++) {
+			equipo.cargarEmpleado( new Empleado( nombres[i], rolesDeEmpleados[i], calificaciones[i] ) );
+		}
+	}
+	
+	// cargarIncompatibles()
+	@Test(expected = Exception.class)
+	public void cargarIncompatiblesNombresIguales() {
+		String nombre1="pepe";
+		String nombre2="pepe";
+		
+		equipo.cargarIncompatibles(nombre1, nombre2);
+	}
+	
+	@Test(expected = Exception.class)
+	public void cargarIncompatiblesNoEmpleado1() {
+		String nombre1="pepe";
+		String nombre2="pepo";
+		
+		equipo.cargarIncompatibles(nombre1, nombre2);
+	}
+	
+	@Test
+	public void cargarIncompatiblesCorrecto() {
+		String nombre1 = "pipo";
+		String nombre2 = "picasso";
+		
+		equipo.cargarIncompatibles(nombre1, nombre2);
+		System.out.println( equipo.getIncompatibilidades().get(0).get(1) );
+		String expect1 = "\nARQUITECTO -- Nombre: pipo, Calificacion: 4";
+		String expect2 = "\nPROGRAMADOR -- Nombre: picasso, Calificacion: 3";
+		
+		assertTrue( expect1.equals( equipo.getIncompatibilidades().get(0).get(0).toString() ));
+		assertTrue( expect2.equals( equipo.getIncompatibilidades().get(0).get(1).toString() ));
+	}
+	
+}
