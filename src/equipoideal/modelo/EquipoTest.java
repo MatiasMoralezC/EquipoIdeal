9ed3db80a71f00b448a8295b39d39b11374916c6
maceratomas <maceratomas@gmail.com>
2023-06-06 23:31:38 -0300

    Limpie el archivo de test

--- src/equipoideal/modelo/EquipoTest.java
+++ src/equipoideal/modelo/EquipoTest.java
@@ -13,200 +13,112 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EquipoTest {
-	/*
-	 * void cargarEmpleado(Empleado e) --> hace falta?
-	 * 
-	 * void cargarIncompatibles(String nombre1, String nombre2) -- listo
-	 * 
-	 * boolean verificarEmpleado(String nombre) -- private -- hace falta¿? el de arriba lo controla
-	 * 
-	 * quitarIncompatibilidad(int index) -- listo
-	 * 
-	 * List<Empleado> encontrarEquipoSinConflictos(List<Empleado> empleados,
-	 *  	List<List<Empleado>> incompatibles, HashMap<Rol, Integer> rolesRequeridos) -- luego
-	 *  
-	 * List<Empleado> filtrarEmpleadosNoRequeridos(List<Empleado> empleados,
-	 * 		HashMap<Rol, Integer> rolesRequeridos)
-	 * 
-	 * int contarEmpleadosRequeridosCumplidos(HashMap<Rol, Integer> rolesRequeridos, List<Empleado> equipo)
-	 * 
-	 * double obtenerCalificacionTotal(List<Empleado> equipo) -- listo
-	 * 
-	 * List<List<Empleado>> generarCombinaciones(List<Empleado> empleados, int r)
-	 * 
-	 * void generarCombinacionesRecursivo(List<Empleado> empleados, int r, int index, List<Empleado> combinacionActual, List<List<Empleado>> result)
-	 * 
-	 * boolean tieneConflictos(List<Empleado> equipo, List<List<Empleado>> incompatibles)
-	 * 
-	 * boolean verificarRequerimiento(HashMap<Rol, Integer> requerimientos, Empleado e)
-	 * 
-	 * Empleado buscarPorNombre(String nombre) -- listo
-	 */
-	
 
 	Equipo equipo;
+
 	@Before
 	public void inicializar() {
 		equipo = new Equipo();
-		
-		String[] nombres = {"pedro","pipo","picasso","pablo"};
-		Rol[] rolesDeEmpleados = {Rol.LIDER_DE_PROYECTO, Rol.ARQUITECTO, Rol.PROGRAMADOR, Rol.TESTER};
-		int[] calificaciones = {5,4,3,3};
+
+		String[] nombres = { "pedro", "pipo", "picasso", "pablo" };
+		Rol[] rolesDeEmpleados = { Rol.LIDER_DE_PROYECTO, Rol.ARQUITECTO, Rol.PROGRAMADOR, Rol.TESTER };
+		int[] calificaciones = { 5, 4, 3, 3 };
 
 		for (int i = 0; i < nombres.length; i++) {
-			equipo.cargarEmpleado( new Empleado( nombres[i], rolesDeEmpleados[i], calificaciones[i] ) );
+			equipo.cargarEmpleado(new Empleado(nombres[i], rolesDeEmpleados[i], calificaciones[i]));
 		}
 	}
-	
+
 	// cargarIncompatibles
 	@Test(expected = Exception.class)
 	public void cargarIncompatiblesNombresIguales() {
-		String nombre1="pepe";
-		String nombre2="pepe";
-		
+		String nombre1 = "pepe";
+		String nombre2 = "pepe";
+
 		equipo.cargarIncompatibles(nombre1, nombre2);
 	}
-	
+
 	@Test(expected = Exception.class)
 	public void cargarIncompatiblesNoEmpleado1() {
-		String nombre1="pepe";
-		String nombre2="pepo";
-		
+		String nombre1 = "pepe";
+		String nombre2 = "pepo";
+
 		equipo.cargarIncompatibles(nombre1, nombre2);
 	}
-	
+
 	@Test
 	public void cargarIncompatiblesCorrecto() {
 		String nombre1 = "pipo";
 		String nombre2 = "picasso";
-		
+
 		equipo.cargarIncompatibles(nombre1, nombre2);
 		String expect1 = "\nARQUITECTO -- Nombre: pipo, Calificacion: 4";
 		String expect2 = "\nPROGRAMADOR -- Nombre: picasso, Calificacion: 3";
-		
-		assertTrue( expect1.equals( equipo.getIncompatibilidades().get(0).get(0).toString() ));
-		assertTrue( expect2.equals( equipo.getIncompatibilidades().get(0).get(1).toString() ));
+
+		assertTrue(expect1.equals(equipo.getIncompatibilidades().get(0).get(0).toString()));
+		assertTrue(expect2.equals(equipo.getIncompatibilidades().get(0).get(1).toString()));
 	}
-	
 
 	// quitarIncompatibilidad
 	@Test
 	public void quitarIncompatibilidadCorrecto() {
 		String nombre1 = "pipo";
 		String nombre2 = "picasso";
-		
+
 		equipo.cargarIncompatibles(nombre1, nombre2);
 		equipo.quitarIncompatibilidad(0);
-		
-		assertTrue( equipo.getIncompatibilidades().isEmpty() );
+
+		assertTrue(equipo.getIncompatibilidades().isEmpty());
 	}
 
 	@Test(expected = Exception.class)
 	public void quitarIncompatibilidadIndexNegativo() {
 		String nombre1 = "pipo";
 		String nombre2 = "picasso";
-		
+
 		equipo.cargarIncompatibles(nombre1, nombre2);
 		equipo.quitarIncompatibilidad(-10);
-		
-		assertTrue( equipo.getIncompatibilidades().isEmpty() );
+
+		assertTrue(equipo.getIncompatibilidades().isEmpty());
 	}
 
 	@Test(expected = Exception.class)
 	public void quitarIncompatibilidadIndexNoValido() {
 		String nombre1 = "pipo";
 		String nombre2 = "picasso";
-		
+
 		equipo.cargarIncompatibles(nombre1, nombre2);
 		equipo.quitarIncompatibilidad(10);
 	}
-	
-	/*
-	 * List<Empleado> filtrarEmpleadosNoRequeridos(List<Empleado> empleados,
-	 * 		HashMap<Rol, Integer> rolesRequeridos)
-	 */
-	
-	
-	
 
-	// double obtenerCalificacionTotal(List<Empleado> equipo)
+	// obtenerCalificacionTotal
 	@Test
-	public void obtenerCalificacionTotalCorrecto(){
+	public void obtenerCalificacionTotalCorrecto() {
 		List<Empleado> aux = new ArrayList<Empleado>();
-		aux.add( new Empleado("paula", Rol.LIDER_DE_PROYECTO, 5) );
-		aux.add( new Empleado("pilar", Rol.ARQUITECTO, 5) );
-		
-		assertTrue( 10.0 == equipo.calcularCalificacionTotal(aux) );
-		
-	}
-	
-	@Test
-	public void obtenerCalificacionTotalVacio(){
-		List<Empleado> aux = new ArrayList<Empleado>();
-		assertTrue( 0.0 == equipo.calcularCalificacionTotal(aux) );
-		
-	}
-	
-//	List<List<Empleado>> generarCombinaciones(List<Empleado> empleados, int r)
-	@Test(expected = Exception.class)
-	public void generarCombinacionesSinEmpleados() {
-		equipo.generarCombinaciones(new ArrayList<>(), 0);
-	}
-	
-	@Test(expected = Exception.class)
-	public void generarCombinacionesEmpleadosNull() {
-		equipo.generarCombinaciones(null, 0);
-	}
-	
-	@Test(expected = Exception.class)
-	public void generarCombinacionesCantMayor() {
-		List empleados = new ArrayList<>();
-		List<Empleado> aux = new ArrayList<Empleado>();
-		aux.add( new Empleado("paula", Rol.LIDER_DE_PROYECTO, 5) );
-		aux.add( new Empleado("pilar", Rol.ARQUITECTO, 5) );
-		
-		equipo.generarCombinaciones(aux, 3);
+		aux.add(new Empleado("paula", Rol.LIDER_DE_PROYECTO, 5));
+		aux.add(new Empleado("pilar", Rol.ARQUITECTO, 5));
+
+		assertTrue(10.0 == equipo.calcularCalificacionTotal(aux));
+
 	}
 
-	// --------- FALTA TESTEAR
-//	@Test
-//	public void generarCombinacionesCorrecto() {
-//		List empleados = new ArrayList<>();
-//		List<Empleado> aux = new ArrayList<Empleado>();
-//		aux.add( new Empleado("paula", Rol.LIDER_DE_PROYECTO, 5) );
-//		aux.add( new Empleado("pilar", Rol.ARQUITECTO, 5) );
-//		aux.add( new Empleado("pepita", Rol.TESTER, 5) );
-//		
-//		System.out.println(equipo.generarCombinaciones(aux, 3));
-//		
-//		
-//	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@Test
+	public void obtenerCalificacionTotalVacio() {
+		List<Empleado> aux = new ArrayList<Empleado>();
+		assertTrue(0.0 == equipo.calcularCalificacionTotal(aux));
+
+	}
+
 	// Empleado buscarPorNombre(String nombre)
-		@Test
-		public void buscarPorNombreIncorrecto() {
-			assertTrue( equipo.buscarPorNombre("pepe") == null );
-		}
-		
-		@Test
-		public void buscarPorNombreCorrecto() {
-			String expect = "\nARQUITECTO -- Nombre: pipo, Calificacion: 4";
-			assertTrue( expect.equals( equipo.buscarPorNombre("pipo").toString() ) );
-		}
-	
+	@Test
+	public void buscarPorNombreIncorrecto() {
+		assertTrue(equipo.buscarPorNombre("pepe") == null);
+	}
+
+	@Test
+	public void buscarPorNombreCorrecto() {
+		String expect = "\nARQUITECTO -- Nombre: pipo, Calificacion: 4";
+		assertTrue(expect.equals(equipo.buscarPorNombre("pipo").toString()));
+	}
+
 }
